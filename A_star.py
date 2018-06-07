2688407cb52298fec06370136b178d24dd646fc3
Ehsan Ahmadi Kian <ehsan_amd72@hotmail.com>
2018-06-07 18:28:42 +0430

    Add files via upload
    Romanian map problem

---
+++ A_star.py
@@ -0,0 +1,63 @@
+﻿print """A* search:
+               Oradea to Craiova"""
+#فراخواني نقشه و هيورستيک از ماژول تعريف شده
+from ROMANIA import MAP, SLD
+#فراخواني ماژول براي کار با صف
+import heapq
+#تعريف کلاس براي کار نقشه
+class PriorityQueue:
+
+    #تعريف صف اوليه خالي 
+    def __init__(self):
+        self.elements = []
+        
+    #خالي کردن صف
+    def empty(self):
+        return len(self.elements) == 0
+
+    #افزودن به صف 
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    #پاپ کرده سپس برميگرداند
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+#تعريف تابع هيورستيک
+def h(edge):
+	return SLD[edge]
+
+#نشان دادن مسير حرکت
+def traverse(parent, start, goal):
+    path = [goal]
+    while path[-1] != start:
+        path.append (parent[path[-1]])
+    path.reverse()
+    return path
+
+
+def astar(G, start, goal):
+	frontier = PriorityQueue()
+	parent 	 =  {}
+	visited  = set()
+	#جمع کردن هزينه
+	frontier.put (start, 0)
+	#شهرهاي ملاقات شده
+	visited.add(start)
+	while not frontier.empty():
+		vertex = frontier.get()
+		if vertex == goal:
+			return traverse(parent, start, goal)
+		for edge in G[vertex]:
+			if edge not in visited:				
+				cost = h(edge) + G[vertex][edge]
+				frontier.put(edge, cost)
+				parent[edge] = vertex
+				visited.add(edge)
+	return None
+
+if __name__ == '__main__':
+	print astar(MAP, "Oradea", 'Craiova')
+	
+
+
---
+++ ROMANIA.py
@@ -0,0 +1,38 @@
+
+
+MAP = {
+	"Arad": 			{"Zerind": 75, "Timisoara": 118, "Sibiu": 140 },
+	"Zerind": 			{"Arad": 75, "Oradea": 71},
+	"Oradea": 			{"Zerind": 71, "Sibiu": 151},
+	"Timisoara":		{"Arad": 118, "Lugoj": 111},
+	"Sibiu":			{"Arad": 140, "Oradea": 151, "Fagaras": 99, "Rimnicu Vilcea": 80},
+	"Fagaras": 			{"Sibiu": 99, "Bucharest": 211},
+	"Rimnicu Vilcea":	{"Sibiu": 80, "Pitesti": 97, "Craiova": 146},
+	"Lugoj":			{"Timisoara": 111, "Mehadia": 70},
+	"Mehadia": 			{"Lugoj": 70, "Drobeta": 75},
+	"Drobeta": 			{"Mehadia": 75, "Craiova": 120},
+	"Craiova": 			{"Drobeta": 120, "Rimnicu Vilcea": 146, "Pitesti": 138},
+	"Pitesti": 			{"Craiova": 138, "Rimnicu Vilcea": 97, "Bucharest": 101},
+	"Bucharest": 		{"Giurgiu": 90, "Pitesti": 101, "Fagaras": 211, "Urziceni": 85},
+	"Giurgiu": 			{"Bucharest": 90},
+	"Urziceni": 		{"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
+	"Hirsova": 			{"Urziceni": 98, "Eforie": 86},
+	"Eforie": 			{"Hirsova": 86},
+	"Vaslui": 			{"Urziceni": 142, "Iasi": 92},
+	"Iasi": 			{"Vaslui": 92, "Neamt": 87},
+	"Neamt":			{"Iasi": 87}
+}
+
+SLD = {
+	'Arad': 366, 'Mehadia': 241,
+	'Bucharest': 0, 'Neamt': 234,
+	'Craiova': 160, 'Oradea': 380,
+	'Drobeta': 242, 'Pitesti': 100,
+	'Eforie': 161, 'Rimnicu Vilcea': 193,
+	'Fagaras': 176, 'Sibiu': 253,
+	'Giurgiu': 77, 'Timisoara': 329,
+	'Hirsova': 151, 'Urziceni': 80,
+	'Iasi': 226, 'Vaslui': 199,
+	'Lugoj': 244, 'Zerind': 374
+}
+
